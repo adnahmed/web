46326a47e379f9228972c5d20e591b5f5fb262f9
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-27 21:18:42 +0500

    import ValidationResult from express-validator

--- auth/login-router.js
+++ auth/login-router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 const secret = require('../config').secret;
