eb3f5bc0bbbda4d53a04976130c544c841dd18ee
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 03:12:30 +0500

    Add error code for duplicate value

--- queries/examinee.js
+++ queries/examinee.js
@@ -1,5 +1,5 @@
 const AllQueries = {
-    createExaminee: "create table examinees (examinee_id bigserial primary key, administor_id bigint references administrators, username varchar(255) unique, password varchar(255), first_name varchar(255), last_name varchar(255))",
+    createExaminee: "create if not exists table examinees (examinee_id bigserial primary key, administor_id bigint references administrators, username varchar(255) unique, password varchar(255), first_name varchar(255), last_name varchar(255))",
     truncateExaminees: "truncate table examinees",
 };
 module.exports = AllQueries;
