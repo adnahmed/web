5354146f22f9137f25f5356cba6cf709e6714e18
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-29 15:46:34 +0500

    Add create administrator database query

--- queries/admin.js
+++ queries/admin.js
@@ -1,4 +1,6 @@
 const AllQueries = {
+    createAdministrator: "Create table administrators (administrator_id bigserial primary key, \
+        username varchar(50) not null unique, password varchar(50) not null)",
     insertAdministrator: "Insert into administrators(username, password) values ($1, $2)",
     insertAdministratorReturning: "Insert into administrators(username, password) values ($1, $2) RETURNING *"
 }
