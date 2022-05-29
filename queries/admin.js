4d03d88a020ffb35f1fd164a06860a2f45632c59
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 03:11:40 +0500

    create dict queries for admin

--- queries/admin.js
+++ queries/admin.js
@@ -1,11 +1,15 @@
 const AllQueries = {
-    createAdministrator: "Create table administrators (administrator_id bigserial primary key, \
+    createAdministrator: "Create table  if not exists administrators (administrator_id bigserial primary key, \
         username varchar(50) not null unique, password text not null)",
-    insertAdministrator: "Insert into administrators(username, password) values ($1, $2)",
-    insertAdministratorReturning: "Insert into administrators(username, password) values ($1, $2) RETURNING *",
-    selectUsernamePassword: "Select username, password from administrators",
-    selectUsernamePasswordWhereUsername: "Select username, password from administrators where username = $1::text",
-    selectPasswordWhereAdministratorId: "Select password from administrators where administrator_id = $1",
+    insert : {
+        usernamePassword: "Insert into administrators(username, password) values ($1, $2)",
+        usernamePasswordReturning: "Insert into administrators(username, password) values ($1, $2) RETURNING *",
+    },
+    select : {
+        usernamePassword: "select username, password from administrators",
+        usernamePasswordWhereUsername: "select username, password from administrators where username = $1::text",
+        passwordWhereAdministratorId: "Select password from administrators where administrator_id = $1",
+    },
     truncateAdministrators: "truncate table administrators",
 }
 
