dd2f19e9f9d0f530950623919435a2c816c6e606
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-29 15:59:00 +0500

    Use adminQueries in tests and import pool without dict

--- db/inmemdb.test.js
+++ db/inmemdb.test.js
@@ -1,18 +1,20 @@
 const { expressjwt } = require('express-jwt');
-const { pool }  = require('./inmemdb');
+const adminQueries = require('../queries/admin');
+
+const pool   = require('./inmemdb');
 describe("Test In Memory Database Initialization/Deinitialization", () => {
     beforeAll(() => {
-        pool.query("Insert into rps.administrators (username, password) values ('admin12', 'admin1234!!')");   
-        pool.query("Insert into rps.administrators (username, password) values ('admin1', 'admin1234!!')");        
+        pool.query(adminQueries.insertAdministrator, ['admin1', 'admin1234!!']);   
+        pool.query(adminQueries.insertAdministrator, ["admin1", "admin123@!"]);       
     });
-    test('Verify if rps.administrator table is created and populated', async () => {
-        let res = await pool.query('Select username, password from rps.administrators');
+    test('Verify if administrator table is created and populated', async () => {
+        let res = await pool.query(adminQueries.selectUsernamePassword);
         expect(res).not.toBeNull();
         expect(res).not.toBeUndefined();
     });
 
     test('Verify if row for username admin1 exists in rps.administrators', async () => {
-        let res = await pool.query('Select username, password from rps.administrators where username = $1::text', ["admin1"]);
+        let res = await pool.query(adminQueries.selectUsernamePasswordWhereUsername, ["admin1"]);
         expect(res).not.toBeNull();
         expect(res).not.toBeUndefined();
         expect(res.rows[0].username).toBe('admin1')
@@ -20,14 +22,14 @@ describe("Test In Memory Database Initialization/Deinitialization", () => {
     });
 
     afterAll(()=> {
-        pool.query("truncate rps.administrators")
+        pool.query(adminQueries.truncateAdministrators);
     });
 });
 
 
 test('Verify if rps.administrators table is dropped properly', async () => {
         try{
-            await pool.query('Select username, password from rps.administrators');
+            await pool.query(adminQueries.selectUsernamePassword);
         } catch(err){
             expect(err).not.toBeNull();
         }
