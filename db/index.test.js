6a1b3e5fac6b8d59d6b40bdb1998d4bdb6e44e5a
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 04:59:27 +0500

    Fix async/await with afterAll and beforeAll

--- db/index.test.js
+++ db/index.test.js
@@ -1,38 +1,40 @@
 const { expressjwt } = require('express-jwt');
 const  pool   = require('./index');
 const adminQueries = require('../queries/admin');
+const { protocols } = require('superagent');
 
 describe("Test Database Initialization/Deinitialization", () => {
+/* truncate because we do insertions in all tests */
+    beforeEach(async () => {
+    await pool.query(adminQueries.truncateAdministrators);
+})
+    /* Database must exist before testing insertion/deletion. */
     describe("Test if administrator was populated correctly", () => {
-        beforeAll(() => {
-            pool.query(adminQueries.insert.usernamePassword, ['admin1', 'admin1234!!']);   
-            pool.query(adminQueries.insert.usernamePassword, ["adminqw1", "admin123@!"]);       
-        });
         test('Verify if administrator table is created and populated', async () => {
+            await pool.query(adminQueries.insert.usernamePassword, ["adminqw1", "admin123@!"]);
             let res = await pool.query(adminQueries.select.usernamePassword);
             expect(res).not.toBeNull();
-            expect(res).not.toBeUndefined();
+            expect(res.rows.length).toBe(1);
         });
     
         test('Verify if row for username admin1 exists in administrators', async () => {
+            await pool.query(adminQueries.insert.usernamePassword, ["admin1", "admin123@!"]);
             let res = await pool.query(adminQueries.select.usernamePasswordWhereUsername, ["admin1"]);
             expect(res).not.toBeNull();
             expect(res).not.toBeUndefined();
             expect(res.rows[0].username).toBe('admin1')
-            expect(res.rows[0].password).toBe('admin1234!!');
+            expect(res.rows[0].password).toBe('admin123@!');
         });
-    
-        afterAll(()=> {
-            pool.query(adminQueries.truncateAdministrators);
-        });
+        /* Each Test inserts similar objects, so we truncate */
+        afterEach(async () => {
+            await pool.query(adminQueries.truncateAdministrators);
+        })
     })
-    test('Verify if administrators table is dropped properly', async () => {
-        try{
-            await pool.query(adminQueries.select.usernamePassword);
-        } catch(err){
-            expect(err).not.toBeNull();
-    }
-});
+    test('Verify if administrators table is truncated', async () => {
+        await pool.query(adminQueries.truncateAdministrators);
+        let res = await pool.query(adminQueries.select.usernamePassword);
+        expect(res.rows.length).toBe(0);
+    });
 });
 
 
