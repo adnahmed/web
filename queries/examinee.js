1280417a7e8848f1dde963211a7196b2867cb7c5
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 00:15:28 +0500

    Add create and truncate queries for examinees

--- queries/examinee.js
+++ queries/examinee.js
@@ -1,5 +1,5 @@
 const AllQueries = {
-    createExaminee: "create table examinee (username varchar(255) primary key, password varchar(255), first_name varchar(255), last_name varchar(255))",
-    truncateExaminees: "truncate table examinee",
+    createExaminee: "create table examinees (examinee_id bigserial primary key, administor_id bigint references administrators, username varchar(255) unique, password varchar(255), first_name varchar(255), last_name varchar(255))",
+    truncateExaminees: "truncate table examinees",
 };
 module.exports = AllQueries;
