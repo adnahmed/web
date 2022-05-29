a08ef811e6c1600454d1a10c90505f1967ce115c
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-29 15:59:20 +0500

    Add select and truncate queries for admin

--- queries/admin.js
+++ queries/admin.js
@@ -2,7 +2,10 @@ const AllQueries = {
     createAdministrator: "Create table administrators (administrator_id bigserial primary key, \
         username varchar(50) not null unique, password varchar(50) not null)",
     insertAdministrator: "Insert into administrators(username, password) values ($1, $2)",
-    insertAdministratorReturning: "Insert into administrators(username, password) values ($1, $2) RETURNING *"
+    insertAdministratorReturning: "Insert into administrators(username, password) values ($1, $2) RETURNING *",
+    selectUsernamePassword: "Select username, password from administrators",
+    selectUsernamePasswordWhereUsername: "Select username, password from administrators where username = $1::text",
+    truncateAdministrators: "truncate table administrators",
 }
 
 module.exports = AllQueries;
