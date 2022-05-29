965e3eb5a1e103be721ea88cc7e26d09e8a8bc28
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-29 15:21:02 +0500

    Add Insert Administrator queries

--- queries/admin.js
+++ queries/admin.js
@@ -1,4 +1,6 @@
 const AllQueries = {
+    insertAdministrator: "Insert into administrators(username, password) values $1, $2",
+    insertAdministratorReturning: "Insert into administrators(username, password) values $1, $2 RETURNING *"
 }
 
 module.exports = AllQueries;
