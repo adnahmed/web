5b1e231e7b00dbac7f2cca917cac13e4171b18cc
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-29 23:38:20 +0500

    Add select password by administrator_id query

--- queries/admin.js
+++ queries/admin.js
@@ -1,10 +1,11 @@
 const AllQueries = {
     createAdministrator: "Create table administrators (administrator_id bigserial primary key, \
-        username varchar(50) not null unique, password varchar(50) not null)",
+        username varchar(50) not null unique, password text not null)",
     insertAdministrator: "Insert into administrators(username, password) values ($1, $2)",
     insertAdministratorReturning: "Insert into administrators(username, password) values ($1, $2) RETURNING *",
     selectUsernamePassword: "Select username, password from administrators",
     selectUsernamePasswordWhereUsername: "Select username, password from administrators where username = $1::text",
+    selectPasswordWhereAdministratorId: "Select password from administrators where administrator_id = $1",
     truncateAdministrators: "truncate table administrators",
 }
 
