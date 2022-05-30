72659d984977c812a179f07d7fa6820891cb3b4b
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 10:44:30 +0500

    Add Model for Examinees

---
+++ models/examinee.js
@@ -0,0 +1,31 @@
+class Examinee {
+    constructor(administrator, username, password, first_name, last_name){
+        this.administrator = administrator;
+        this.username = username;
+        this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+    administrator(newadministrator) {
+        this.administrator = newadministrator;
+        return this;
+    }
+    first_name(newFirstName) {
+        this.first_name = newFirstName;
+        return this;
+    }
+    last_name(newLastName) {
+        this.last_name = newLastName;
+        return this;
+    }
+    username(newusername) {
+        this.username = newusername;
+        return this;
+    }
+    password(newpassword) {
+        this.password = newpassword;
+        return this;
+    }
+}
+
+module.exports = Examinee;
