f8404e9780a4d20bcf42f0be2348bd2c315b5ba0
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 05:19:04 +0500

    We want to cacade changes if all examinees are removed

--- queries/examinee.js
+++ queries/examinee.js
@@ -1,6 +1,6 @@
 const AllQueries = {
     createExaminee: "create if not exists table examinees (examinee_id bigserial primary key, administor_id bigint references administrators, username varchar(255) unique, password varchar(255), first_name varchar(255), last_name varchar(255))",
-    truncateExaminees: "truncate table examinees",
+    truncateExaminees: "truncate table examinees cascade",
     select: {
         administratorUsernamePassword: "select administrator, username, password from examinees",
     },
