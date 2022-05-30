05135effd2ad9287b599b705355a8fb63e245270
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 09:18:43 +0500

    fix create examinee query and insert username, password query

--- queries/examinee.js
+++ queries/examinee.js
@@ -1,12 +1,16 @@
 const AllQueries = {
-    createExaminee: "create if not exists table examinees (examinee_id bigserial primary key, administor_id bigint references administrators, username varchar(255) unique, password varchar(255), first_name varchar(255), last_name varchar(255))",
+    
+    createExaminee: "create table if not exists examinees (examinee_id bigserial primary key not null, administrator bigint references administrators(administrator_id), username varchar(255) unique not null, \
+        password varchar(255) not null, first_name varchar(255), last_name varchar(255), name text generated always as \
+        (first_name || ' ' || last_name) stored)",
+    
     truncateExaminees: "truncate table examinees cascade",
     select: {
         administratorUsernamePassword: "select administrator, username, password from examinees",
     },
     insert: {
-        usernamePassword: "insert into examinees (administerator, username, password) values ($1, $2, $3)",
-        usernamePasswordReturning: "insert into examinees (administerator, username, password) values ($1, $2, $3) RETURNING *"
+        usernamePassword: "insert into examinees (administrator, username, password) values ($1, $2, $3)",
+        usernamePasswordReturning: "insert into examinees (administrator, username, password) values ($1, $2, $3) RETURNING *"
     }
 };
 module.exports = AllQueries;
