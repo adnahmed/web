a9cfe9c2f4864d37b0cde77b0868cc5cefa2ab57
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 10:42:34 +0500

    Insert usernamePasswordName query without return for admins

--- queries/admin.js
+++ queries/admin.js
@@ -5,6 +5,7 @@ const AllQueries = {
     insert : {
         usernamePassword: "Insert into administrators(username, password) values ($1, $2)",
         usernamePasswordReturning: "Insert into administrators(username, password) values ($1, $2) RETURNING *",
+        usernamePasswordName: "Insert into administrators(username, password, first_name, last_name) values ($1, $2, $3, $4) ",
         usernamePasswordNameReturning: "Insert into administrators(username, password, first_name, last_name) values ($1, $2, $3, $4) RETURNING *",
     },
     select : {
