53081662745401839e8e588d03beb9cb3c9fd11a
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 10:03:27 +0500

    Make registeration of administrator require first_name, last_name

--- models/administrator.js
+++ models/administrator.js
@@ -1,7 +1,17 @@
 class Administrator {
-    constructor(username, password){
+    constructor(username, password, first_name, last_name){
         this.username = username;
         this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+    first_name(newFirstName) {
+        this.first_name = newFirstName;
+        return this;
+    }
+    last_name(newLastName) {
+        this.last_name = newLastName;
+        return this;
     }
     username(newusername) {
         this.username = newusername;
--- queries/admin.js
+++ queries/admin.js
@@ -1,9 +1,11 @@
 const AllQueries = {
     createAdministrator: "Create table  if not exists administrators (administrator_id bigserial primary key, \
-        username varchar(50) not null unique, password text not null)",
+        username varchar(50) not null unique, password text not null, first_name varchar(255) not null, last_name varchar(255) not null, name text generated always as \
+        (first_name || ' ' || last_name) stored)",
     insert : {
         usernamePassword: "Insert into administrators(username, password) values ($1, $2)",
         usernamePasswordReturning: "Insert into administrators(username, password) values ($1, $2) RETURNING *",
+        usernamePasswordNameReturning: "Insert into administrators(username, password, first_name, last_name) values ($1, $2, $3, $4) RETURNING *",
     },
     select : {
         usernamePassword: "select username, password from administrators",
