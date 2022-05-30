ad9c09abf29c92c6fbe398be6b01d9614b4c7a47
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 05:04:28 +0500

    Add examinee Queries

--- queries/examinee.js
+++ queries/examinee.js
@@ -1,5 +1,11 @@
 const AllQueries = {
     createExaminee: "create if not exists table examinees (examinee_id bigserial primary key, administor_id bigint references administrators, username varchar(255) unique, password varchar(255), first_name varchar(255), last_name varchar(255))",
     truncateExaminees: "truncate table examinees",
+    select: {
+        administratorUsernamePassword: "select administrator, username, password from examinees",
+    },
+    insert: {
+        examineeUsernamePassword: "insert into examinees (administerator, username, password) values ($1, $2, $3)"
+    }
 };
 module.exports = AllQueries;
