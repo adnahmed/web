a55d0ae95f8170e3807f57b1f326530f4812ffeb
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-30 09:31:03 +0500

    make last name and fistname not null for examinees

--- queries/examinee.js
+++ queries/examinee.js
@@ -1,7 +1,7 @@
 const AllQueries = {
     
     createExaminee: "create table if not exists examinees (examinee_id bigserial primary key not null, administrator bigint references administrators(administrator_id), username varchar(255) unique not null, \
-        password varchar(255) not null, first_name varchar(255), last_name varchar(255), name text generated always as \
+        password varchar(255) not null, first_name varchar(255) not null, last_name varchar(255) not null, name text generated always as \
         (first_name || ' ' || last_name) stored)",
     
     truncateExaminees: "truncate table examinees cascade",
