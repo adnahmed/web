ec943725cacc8926c7671f103849686853455821
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-06-10 12:54:27 +0500

    standard sequelize-cli model format

--- models/proctor.js
+++ models/proctor.js
@@ -1,49 +1,52 @@
-const { DataTypes, Model } = require('sequelize');
-const sequelize = require('../db/index');
-class Proctor extends Model { 
-    getFullname() {
-        return [this.first_name, this.last_name].join(' ');
-    }
-    static associate(models) {
-        Proctor.belongsTo(models.Administrator);
-    }
-}
-Proctor.init({
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    first_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-        }
-    },
-    last_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-        }
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-        }
-    },
-}, {
-    sequelize,
-    modelName: 'proctor',
-    timestamps: false,
-});
+'use strict';
+const {
+  Model
+} = require('sequelize');
 
-
-module.exports = Proctor;
+module.exports = (sequelize, DataTypes) => {
+    class Proctor extends Model { 
+        getFullname() {
+            return [this.first_name, this.last_name].join(' ');
+        }
+        static associate(models) {
+            Proctor.belongsTo(models.Administrator);
+        }
+    }
+    Proctor.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            }
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            }
+        },
+    }, {
+        sequelize,
+        modelName: 'proctor',
+        timestamps: false,
+    });
+  return Proctor;
+};
