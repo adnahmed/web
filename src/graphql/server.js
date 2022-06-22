6cfa20b297c3a235a8c7eae09e5e26c8575022ee
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-06-22 12:50:14 +0500

    apollo server is good

--- src/graphql/server.js
+++ src/graphql/server.js
@@ -1,23 +1,17 @@
 const { ApolloServer } = require("apollo-server-express");
-const { loadFiles } = require('@graphql-tools/load-files')
+const { loadFiles } = require('@graphql-tools/load-files');
+const { mergeResolvers } = require('@graphql-tools/merge');
 const { typeDefs: scalarTypeDefs } = require('graphql-scalars');
 const { resolvers: scalarResolvers } = require('graphql-scalars');
+const { print }  = require('graphql');
 const path = require('path');
+
 module.exports = (async()=> {
-  const typeDefs = (await loadFiles(path.join(__dirname, './typedefs/**/*.graphql'))) + '\r\n' + scalarTypeDefs;
-  const myResolvers = {
-    Query: {
-        books: () => books,
-    },
-}
-  //await loadFiles('resolvers/**/*.js');
-  const allResolvers = {...myResolvers, ...scalarResolvers};
-  const resolvers = allResolvers;
-  const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers,
-    csrfPrevention: true,
-    cache: "bounded",
-  }); 
+const server = new ApolloServer({
+  typeDefs: print(await loadFiles(path.join(__dirname, './typedefs/**/*.graphql'))) + scalarTypeDefs,
+  resolvers: {...mergeResolvers(await loadFiles(path.join(__dirname,'./resolvers/**/*.js'))), ...scalarResolvers},
+  csrfPrevention: true,
+  cache: "bounded",
+}); 
   return server
 })();
