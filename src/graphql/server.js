98fa70dcf16f74e7ac68daf84ad7c0f980e42b65
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-06-25 17:17:14 +0500

    We cannot send any object to the closure inside passport

--- src/graphql/server.js
+++ src/graphql/server.js
@@ -1,31 +1,38 @@
-const { ApolloServer, AuthenticationError } = require("apollo-server-express");
-const { loadFiles } = require('@graphql-tools/load-files');
-const { mergeResolvers } = require('@graphql-tools/merge');
-const { typeDefs: scalarTypeDefs } = require('graphql-scalars');
-const { resolvers: scalarResolvers } = require('graphql-scalars');
-const { print }  = require('graphql');
-const jwt = require("jsonwebtoken");
-const path = require('path');
-const passport = require("passport");
+const { ApolloServer, AuthenticationError } = require('apollo-server-express')
+const { loadFiles } = require('@graphql-tools/load-files')
+const { mergeResolvers } = require('@graphql-tools/merge')
+const { typeDefs: scalarTypeDefs } = require('graphql-scalars')
+const { resolvers: scalarResolvers } = require('graphql-scalars')
+const { print } = require('graphql')
+const jwt = require('jsonwebtoken')
+const path = require('path')
+const passport = require('passport')
+const { CLIENT_RENEG_WINDOW } = require('tls')
 
-module.exports = (async()=> {
-const server = new ApolloServer({
-  resolvers: {
-    ...mergeResolvers(await loadFiles(path.join(__dirname,'./resolvers/**/*.js'))),
-    ...scalarResolvers},
-  typeDefs: 
-    print(await loadFiles(path.join(__dirname, './typedefs/**/*.graphql'))) +
-    scalarTypeDefs,
-  csrfPrevention: true,
-  cache: "bounded",
-  context: ({req}) => {
-    req.headers.authorization = req.headers.authorization || "";
-    req.headers.authorization = "Bearer " + req.headers.authorization;
-    passport.authenticate(['administrator', 'proctor', 'examinee'], { session: false}, (err,user, role)=>{
-      if (err) return new AuthenticationError(err);
-      return { user , role};
-    })(req);
-  }
-}); 
-  return server
-})();
+module.exports = (async () => {
+    const server = new ApolloServer({
+        resolvers: {
+            ...mergeResolvers(
+                await loadFiles(path.join(__dirname, './resolvers/**/*.js'))
+            ),
+            ...scalarResolvers,
+        },
+        typeDefs:
+            print(
+                await loadFiles(path.join(__dirname, './typedefs/**/*.graphql'))
+            ) + scalarTypeDefs,
+        csrfPrevention: true,
+        cache: 'bounded',
+        context: ({ req }) => {
+            req.headers.authorization = req.headers.authorization || ''
+            req.headers.authorization = 'Bearer ' + req.headers.authorization
+            var res =  passport.authenticate(
+              ['administrator', 'proctor', 'examinee'],
+              { session: false },
+              
+          )(req);
+          return res;
+          },
+    })
+    return server
+})()
