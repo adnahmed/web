a83cb8e36a2581638ffa3c61dcb868127eda091f
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-06-27 00:13:45 +0500

    fix error handling in server.js for context

--- src/graphql/server.js
+++ src/graphql/server.js
@@ -1,13 +1,14 @@
-const { ApolloServer, AuthenticationError } = require('apollo-server-express');
-const { loadFiles } = require('@graphql-tools/load-files');
-const { mergeResolvers } = require('@graphql-tools/merge');
-const { typeDefs: scalarTypeDefs } = require('graphql-scalars');
-const { resolvers: scalarResolvers } = require('graphql-scalars');
-const { print, getIntrospectionQuery } = require('graphql');
-const jwt = require('jsonwebtoken');
-const secret = require('../config').secret;
-const path = require('path');
-const { getUser } = require('../lib/getter');
+const { ApolloServer, AuthenticationError } = require('apollo-server-express')
+const { loadFiles } = require('@graphql-tools/load-files')
+const { mergeResolvers } = require('@graphql-tools/merge')
+const { typeDefs: scalarTypeDefs } = require('graphql-scalars')
+const { resolvers: scalarResolvers } = require('graphql-scalars')
+const { print, getIntrospectionQuery } = require('graphql')
+const jwt = require('jsonwebtoken')
+const secret = require('../config').secret
+const path = require('path')
+const { getUser } = require('../lib/getter')
+const logger = require('../logger')
 
 module.exports = (async () => {
     const server = new ApolloServer({
@@ -23,15 +24,25 @@ module.exports = (async () => {
             ) + scalarTypeDefs,
         csrfPrevention: true,
         cache: 'bounded',
-        context: ({ req }) => {
-            if (!req.headers.authorization) return;
+        context: async ({ req }) => {
+            if (!req.headers.authorization) return
             try {
-                const decrypt = jwt.verify(req.headers.authorization, secret);
-                return { user: getUser(decrypt), role: decrypt.role }
+                const decrypt = jwt.verify(req.headers.authorization, secret)
+                try {
+                    const user = await getUser(decrypt, req.ip);
+
+                    if (!user)
+                        throw new AuthenticationError(
+                            `${decrypt.role}: ${decrypt.sub} not Found.`
+                        )
+                    return { user, role: decrypt.role }
+                } catch (err) {
+                    logger.info(err)
+                }
             } catch (err) {
-                throw new AuthenticationError("Invalid Token");
+                logger.warn(`Invalid token: ${req.headers.authorization} from ${req.ip}`);
             }
         },
     })
     return server
-})();
+})()
