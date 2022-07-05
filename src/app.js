565dda54e4e6c360c9c036226c049b521db85425
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-07-05 17:15:25 +0500

    neo4j type handler and error handler

--- src/app.js
+++ src/app.js
@@ -6,11 +6,24 @@ app.use(bodyParser.json()); // parse JSON body in POST request body
 app.use(bodyParser.urlencoded({ extended: true })); // parse multipart-form data in POST request body 
 app.use(morgan('combined'));
 
+// Convert response from neo4j types to native types
+app.use(require('./middleware/neo4j-type-handler'))
+
 (async () => {
     const apolloServer = await require('./graphql/server');
     await apolloServer.start();
     apolloServer.applyMiddleware({ app });
 })();
+
+// Handle any constraint errors thrown by Neo4j
+app.use(require('./middleware/neo4j-error-handler'))
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 
 module.exports = app;
 
