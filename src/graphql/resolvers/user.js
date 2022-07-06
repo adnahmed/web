9edef6d2b6e5da691900c937251f5e9aa6a86770
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-07-06 23:30:01 +0500

    we have labels for user types and resolvers to handle user registeration/login

--- src/graphql/resolvers/user.js
+++ src/graphql/resolvers/user.js
@@ -1,85 +1,161 @@
-const authenticator = require('../../lib/Authenticator').defaultAuthenticator;
-const logger = require('../../logger/index');
+const logger = require('../../logger/index')
+const neo4j = require('../../neo4j')
+const errorHandler = require('../../middleware/neo4j-error-handler')
+const cypher = require('../../cypher/index')
+const bcrypt = require('bcrypt')
+const config = require('../../config')
+const jwt = require('jsonwebtoken')
 module.exports = {
-    User: {
-        __resolveType(user, context, info) {
-            
-        }
-    },
     Query: {
-        async loginOther(parent, args, context) {
-            var response = {};
+        async logIn(parent, args, context) {
             try {
-                response.token = await authenticator.loginOther(args.username, args.password, args.role, args.administratorUsername);
-                response.code = 200;
-                response.message = "Login Successful";
-                response.success = true;
-                return response;
-            }
-            catch (err) {
-                response.code = 403;
-                response.message = err.message;
-                response.success = false;
-                response.token = null;
-                return response;
+                const userPasswordRes = await neo4j.read(
+                    cypher(`get-user-password-by-username`),
+                    {
+                        username: args.username,
+                    }
+                )
+                const hashedPassword = userPasswordRes.records[0].get(0)
+                try {
+                    const isSame = await bcrypt.compare(
+                        args.password,
+                        hashedPassword
+                    )
+                    if (!isSame)
+                        return {
+                            code: 403,
+                            message: 'Invalid Username or Password',
+                            success: false,
+                        }
+                    try {
+                        const userRes = await neo4j.read(
+                            cypher(`get-user-by-username`),
+                            { username: args.username }
+                        )
+                        const payload = {
+                            user: userRes.records[0].get(0).properties,
+                            labels: userRes.records[0].get(0).labels,
+                        }
+                        try {
+                            const token = await jwt.sign(payload, config.secret)
+                            return {
+                                code: 200,
+                                message: 'Login Success',
+                                success: true,
+                                token: token,
+                                user: payload.user,
+                            }
+                        } catch (err) {
+                            logger.warn(
+                                `JWT could not be signed, payload: ${payload}`
+                            )
+                            return {
+                                code: 500,
+                                message: 'Internal Server Error',
+                                success: false,
+                            }
+                        }
+                    } catch (err) {
+                        return {
+                            code: 500,
+                            message: `Internal Error, Could not Fetch User ${args.username}`,
+                            success: false,
+                        }
+                    }
+                } catch (err) {
+                    logger.warn(
+                        `bcrypt error comparing password: ${hashedPassword} with ${args.password}`
+                    )
+                    return {
+                        code: 500,
+                        message: 'Internal Error',
+                        success: 'false',
+                    }
+                }
+            } catch (err) {
+                logger.warn(
+                    `Neo4j Error: Could not retrieve username: ${args.username}`
+                )
+                return errorHandler(err)
             }
         },
-        async loginAdministrator(parent, args, context) {
-            var response = {};
-            try {
-                response.token = await authenticator.loginAdministrator(args.username, args.password);
-                response.code = 200;
-                response.message = "Login Successful";
-                response.success = true;
-                return response;
-            }
-            catch (err) {
-                response.code = 403;
-                response.message = err.message;
-                response.success = false;
-                response.token = null;
-                return response;
-            }
-        }
     },
     Mutation: {
-        async registerAdministrator(parent, args, context) {
-            var response = {}
+        async register(parent, args, context) {
+            if (!['administrator', 'examinee', 'proctor'].includes(args.role))
+                return {
+                    code: 400,
+                    message:
+                        'Unknown Role provided. Must be one of [ administrator, proctor, examinee ]',
+                    success: false,
+                }
+            if (arg.role == 'administrator' && !args.organization) {
+                return {
+                    code: 400,
+                    message:
+                        'Organization must be provided for administrator registeration.',
+                    success: false,
+                }
+            }
+            /* Queries expect parameter and throw error for null */
+            args.password = args.password || ''
+            args.prefix = args.prefix || ''
+            args.middle_name = args.middle_name || ''
+            args.last_name = args.last_name || ''
             try {
-                response.user = await authenticator.registerAdministrator(args.username, args.password, args.first_name, args.middle_name, args.last_name)
-                response.code = 200
-                response.message = 'Administrator registered successfully'
-                response.success = true
-                return response
+                const hashedPassword = await bcrypt.hash(
+                    args.password,
+                    config.saltRounds
+                )
+
+                args.password = hashedPassword
+                try {
+                    const res = await neo4j.write(
+                        cypher(`create-user-${args.role}`),
+                        args
+                    )
+
+                    if (args.role == 'administrator') {
+                        await neo4j.write(
+                            cypher('create-administrator-organization-relationship', args)
+                        );
+                    }
+
+                    const payload = {
+                        user: res.records[0].get(0).properties,
+                        labels: res.records[0].get(0).labels,
+                    }
+                    try {
+                        const token = await jwt.sign(payload, config.secret)
+
+                        return {
+                            code: 200,
+                            message: 'Registeration Success',
+                            success: true,
+                            token: token,
+                            user: payload.user,
+                        }
+                    } catch (err) {
+                        logger.warn(
+                            `JWT could not be signed, payload: ${payload}`
+                        )
+                        return {
+                            code: 500,
+                            message: 'Internal Server Error',
+                            success: false,
+                        }
+                    }
+                } catch (err) {
+                    return errorHandler(err)
+                }
             } catch (err) {
-                logger.warn(`Error: ${err.message}`)
-                if (err.code) response.code = err.code;
-                else response.code = 500
-                if (err.message) response.message = err.message
-                else response.message = "Internal Server Error"
-                response.success = false
-                response.user = null
-                return response;
+                logger.warn(`Could not hash given password: ${args.password}`)
+                return {
+                    code: 500,
+                    message: 'Internal Server Error',
+                    success: false,
+                }
             }
         },
-        async registerOther(parent, args, context) {
-            var response = {}
-            try {
-                response.user = await authenticator.registerOther(args.username, args.password, args.first_name, args.middle_name, args.last_name, args.role, args.administratorUsername)
-                response.code = 200
-                response.message = 'Administrator registered successfully'
-                response.success = true
-                return response
-            } catch (err) {
-                logger.warn(`Error: ${err.message}`)
-                if (err.code) response.code = err.code;
-                else response.code = 500
-                if (err.message) response.message = err.message
-                else response.message = "Internal Server Error"
-                response.success = false
-                response.user = null
-                return response;
-            }
-        }
-    }
-};
+    },
+}
