e24327d2bedd4fc4260d186065688fb3e357c031
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-07-29 09:32:50 +0500

    Rate Limiter Redis Configured with Password and Retry Strategy

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
 const express = require('express');
 const app = express();
+
 const neo4j = require('./neo4j');
 const cypher = require('./cypher/index');
+const rateLimiterRedis = require('./middleware/rateLimiterRedis');
 
 app.use(bodyParser.json()); // parse JSON body in POST request body
 app.use(bodyParser.urlencoded({ extended: true })); // parse multipart-form data in POST request body 
 app.use(morgan('combined'));
-
-(async () => {
-    const rateLimiterRedisMiddleware = await require('./middleware/rateLimiterRedis');
-    app.use(rateLimiterRedisMiddleware);
-})();
+app.use(rateLimiterRedis);
 
 (async () => {
     const apolloServer = await require('./graphql/server');
@@ -23,7 +22,6 @@ app.use(morgan('combined'));
 /* Create Constraints on Startup */
 neo4j.write(cypher('user-username-unique-constraint'));
 /*********/
-
 
 module.exports = app;
 
--- src/middleware/rateLimiterRedis.js
+++ src/middleware/rateLimiterRedis.js
@@ -1,36 +1,77 @@
 const redis = require('redis')
 const { RateLimiterRedis } = require('rate-limiter-flexible')
 const logger = require('../logger')
-module.exports = (async () => {
-    const redisClient = redis.createClient({
-        host: 'edis',
+const config = require('../config').redis
+const redisClient = redis.createClient({
+    enable_offline_queue: false,
+    username: config.username,
+    password: config.password,
+    socket: {
+        host: config.REDIS_HOST,
         port: 6379,
-        enable_offline_queue: false,
-    })
+        reconnectStrategy: (numberRetries) => {
+            if (numberRetries > config.MAX_CONNECTION_RETRY) {
+                // End reconnecting with built in error
+                return new Error("Could not Connect to Redis: Max number of retries reached.");
+            }
+            console.log(
+                'Retrying Redis Connection ... Retry#' + numberRetries
+            );
+            // reconnect after
+            return Math.min(numberRetries * 100, config.RECONNECT_TIME)
+        },
+    },
+})
 
-    try {
-        await redisClient.connect()
-        console.log('Redis cache Connected.')
-    } catch (err) {
-        logger.warn(`Redis Connection failed: error ${err.message}`)
-        throw err
-    }
-    const rateLimiter = new RateLimiterRedis({
-        storeClient: redisClient,
-        keyPrefix: 'middleware',
-        points: 100, // 100 requests
-        inmemoryBlockOnConsumed: 100, // no key push to redis after points >= 100
-        duration: 1, // per 1 second for IP
-    })
+redisClient.on('ready', ()=> {
+    console.log("Redis Client Connected.");
+})
 
-    return (req, res, next) => {
-        rateLimiter
-            .consume(req.ip)
-            .then(() => {
-                next()
-            })
-            .catch((err) => {
-                res.status(429).send('Too Many Requests.')
-            })
-    }
-})();
+redisClient.on('error', (err) => {
+    // console.log("Redis:" + err.message);
+    logger.warn(err.message)
+});
+
+var opts, rateLimiterRedis
+function connectToRedis() {
+    redisClient.connect().then(() => {
+        opts = {
+            // Basic options
+            storeClient: redisClient,
+            points: 5, // Number of points
+            duration: 5, // Per second(s)
+
+            // Custom
+            execEvenly: false, // Do not delay actions evenly
+            blockDuration: 0, // Do not block if consumed more than points
+            keyPrefix: 'rlflx', // must be unique for limiters with different purpose
+        }
+        rateLimiterRedis = new RateLimiterRedis(opts)
+    }).catch(err => {
+            console.log(err.message);
+            logger.warn(err.message);
+    })
+}
+connectToRedis()
+function rateLimitMiddleware(req, res, next) {
+    rateLimiterRedis
+        .consume(req.ip)
+        .then((rateLimiterRes) => {
+            // ... Some app logic here ...
+        })
+        .catch((rejRes) => {
+            if (rejRes instanceof Error) {
+                // Some Redis error
+                // Never happen if `insuranceLimiter` set up
+                // Decide what to do with it in other case
+            } else {
+                // Can't consume
+                // If there is no error, rateLimiterRedis promise rejected with number of ms before next request allowed
+                const secs = Math.round(rejRes.msBeforeNext / 1000) || 1
+                res.set('Retry-After', String(secs))
+                res.status(429).send('Too Many Requests')
+            }
+        })
+}
+
+module.exports = rateLimitMiddleware
