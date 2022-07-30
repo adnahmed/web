4267339cf30e32a5d0e0cbcf0617021321e5f20d
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-07-30 15:12:15 +0500

    setup retry for neo4j connectivity on startup

--- src/neo4j.js
+++ src/neo4j.js
@@ -1,20 +1,31 @@
-const neo4j = require('neo4j-driver')
-const config = require('./config')
-const driver = neo4j.driver(config.neo4j.url, neo4j.auth.basic(config.neo4j.username, config.neo4j.password));
-
+const neo4j = require('neo4j-driver');
+const logger = require('./logger');
+const config = require('./config').neo4j;
+const driver = neo4j.driver(config.url, neo4j.auth.basic(config.username, config.password));
 (async () => {
-    try {
-        await driver.verifyConnectivity()
-        console.log('Neo4j Driver created')
-        console.log('Setting up constraints for Database.')
-        require('./cypher/contraints-setup');
-    } catch (error) {
-        console.log(`connectivity verification failed. ${error}`)
+    var retryTime = 0;
+    var connectionSuccessful = false;
+    while(!connectionSuccessful && !retryTime > config.MAX_CONNECTION_RETRY) {
+        try {
+            await driver.verifyConnectivity()
+            connectionSuccessful = true;
+            console.log('Neo4j Driver created')
+            console.log('Setting up constraints for Database.')
+            require('./cypher/contraints-setup');
+        } catch (error) {
+            console.log(`Retry: ${retryTime}, Connectivity verification failed. ${error}`)
+            if(retryTime == config.MAX_CONNECTION_RETRY) {
+                logger.warn(`${Date.now()} : Unable to connect to db on startup with error: ${error} `)
+                throw new Error("Unable to connect to Neo4j. Please start db server to continue.")
+            }
+            retryTime += 1;
+            await new Promise(resolve => setTimeout(resolve, Math.min(5000, config.RECONNECT_TIME * retryTime)));
+        }
     }
 })();
 
 module.exports = {
-    read: (cypher, params = {}, database = config.neo4j.database) => {
+    read: (cypher, params = {}, database = config.database) => {
         const session = driver.session({
             defaultAccessMode: neo4j.session.READ,
             database,
@@ -30,7 +41,7 @@ module.exports = {
                 throw e
             })
     },
-    write: (cypher, params = {}, database = config.neo4j.database) => {
+    write: (cypher, params = {}, database = config.database) => {
         const session = driver.session({
             defaultAccessMode: neo4j.session.WRITE,
             database,
