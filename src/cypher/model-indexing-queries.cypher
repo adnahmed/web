39ed97a93c60ea07b9fdd991d147c5c798995490
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-08-03 03:11:08 +0500

    Indexing Fix

--- src/cypher/model-indexing-queries.cypher
+++
@@ -1,2 +0,0 @@
-create index user_index_username if not exists for (u:User) on (u.username)
-create index organization_index_name if not exists for (o:Organization) on (o.name)
--- src/cypher/startup-setup.js
+++ src/cypher/startup-setup.js
@@ -1,13 +1,20 @@
 const neo4j = require('../neo4j');
 const cypher = require('./index');
-
+const fs = require('fs');
+const path = require('path');
 (async ()=> {
     /* Create Constraints on Startup */
     await neo4j.write(cypher('user-username-unique-constraint'));
     /*********/
 
     /* Create Index on Startup */
-    await neo4j.write(cypher('model-indexing-queries'));
+    fs.readdir(__dirname,null ,async (err, files) => {
+        for (file of files) {
+            let name = file;
+            if (/index[\w-]+.cypher/.test(file))
+                await neo4j.write(cypher(path.basename(file, '.cypher')));    
+        }
+    })
     /*********/
     console.log('Startup setup complete.');
 })();
