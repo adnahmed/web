7168c1008ef022413bf19dad04d9c41fb88b2a0c
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-08-28 14:13:08 +0500

    Add console logger transport in debug mode

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,24 +1,34 @@
-const { createLogger, format, transports } = require('winston');
-const { errors, splat, combine, timestamp, label, printf, json } = format;
-const logger = createLogger({
+const { createLogger, format, transports } = require('winston')
+const winston = require('winston/lib/winston/config')
+const { errors, splat, combine, timestamp, label, printf, json } = format
+const loggerOptions = {
     level: 'info',
     format: combine(
-      timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss'
-      }),
-      errors({ stack: true }),
-      splat(),
-      json()
+        timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        errors({ stack: true }),
+        splat(),
+        json()
     ),
     defaultMeta: { service: 'remote-proctoring-system Backend' },
     transports: [
-      //
-      // - Write to all logs with level `info` and below to `quick-start-combined.log`.
-      // - Write all logs error (and below) to `quick-start-error.log`.
-      //
-      new transports.File({ filename: '../logs/rps-log-error.log', level: 'error' }),
-      new transports.File({ filename: '../logs/rps-log-combined.log' })
-    ]
-  });
+        new transports.File({
+            filename: '../logs/rps-log-warn.log',
+            level: 'warn',
+        }),
+        new transports.File({
+            filename: '../logs/rps-log-error.log',
+            level: 'error',
+        }),
+        new transports.File({ filename: '../logs/rps-log-combined.log' }),
+    ],
+}
+if (process.env.NODE_ENV != "production") {
+    logger.add(winston.transports.Console({
+        format: winston.format.simple(),
+    }))
+}
+const logger = createLogger(loggerOptions)
 
-  module.exports = logger;
+module.exports = logger
