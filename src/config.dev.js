1ed6153a4524c89efb8d9b6b48f3e57b2a4fdbcc
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-09-01 20:11:49 +0500

    fix endpoint graphql in config

--- src/config.dev.js
+++ src/config.dev.js
@@ -1,25 +1,28 @@
-module.exports = {
-    secret: "mysecret",
+const config = {
+    secret: 'mysecret',
     saltRounds: 1,
     port: 4001,
     neo4j: {
-      path: '.env.dev'
+        path: '.env.dev',
     },
     redis: {
-      username: 'default',
-      password: '',
-      REDIS_HOST: 'localhost',
-      REDIS_PORT: 7379,
-      MAX_CONNECTION_RETRY: 10,
-      RECONNECT_TIME: 3000,
+        username: 'default',
+        password: '',
+        REDIS_HOST: 'localhost',
+        REDIS_PORT: 7379,
+        MAX_CONNECTION_RETRY: 10,
+        RECONNECT_TIME: 3000,
     },
     livekit: {
-      apiKey: 'APItrPRpw93WoiW',
-      secretKey: 'CSNYtif1ll1nvoeeqpV7mcMMKI7fQs34vSJ3BQLhUPJA',
-      host: 'http://localhost:8880'
+        apiKey: 'APItrPRpw93WoiW',
+        secretKey: 'CSNYtif1ll1nvoeeqpV7mcMMKI7fQs34vSJ3BQLhUPJA',
+        host: 'http://localhost:8880',
     },
+}
+
+module.exports = {
+    ...config,
     graphql: {
-      endpoint: `http://localhost:${this.port}/graphql`
-    }
-  };
-  
+        endpoint: `http://localhost:${config.port}/graphql`,
+    },
+}
