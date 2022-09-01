7342a3b0698d05619b3d6f3c6c4fd3ef5389ff0a
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-09-01 21:23:49 +0500

    Graphql server seperation from express app.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const config = require('./src/config');
 const app = require('./src/app');
-
+require("./src/graphql/server");
 app.listen(config.port, () => {
     console.log(`Server is listening on port ${config.port}`);
 })
--- src/graphql/server.js
+++ src/graphql/server.js
@@ -1,47 +1,52 @@
 const { ApolloServer } = require('apollo-server-express')
-const { loadFiles } = require('@graphql-tools/load-files')
+const { loadFiles, loadFilesSync } = require('@graphql-tools/load-files')
 const { mergeResolvers } = require('@graphql-tools/merge')
 const { typeDefs: scalarTypeDefs } = require('graphql-scalars')
 const { resolvers: scalarResolvers } = require('graphql-scalars')
 const { print } = require('graphql')
-const depthLimit = require('graphql-depth-limit');
+const depthLimit = require('graphql-depth-limit')
 const path = require('path')
 const logger = require('../logger')
-const { getUser } = require("./auth_utils");
+const { getUser } = require('./auth_utils')
 
-module.exports = (async () => {
-    const server = new ApolloServer({
-        resolvers: {
-            ...scalarResolvers,
-            ...mergeResolvers(
-                await loadFiles(path.join(__dirname, './resolvers/**/*.js'))
-            ),
-        },
-        typeDefs: [
-            print(
-                await loadFiles(path.join(__dirname, './typedefs/**/*.graphql'))
-            ),
-            ...scalarTypeDefs
-        ],
-        csrfPrevention: true,
-        // TODO: Add Proper Error Handling.
-        cache: 'bounded',
-        context: async ({ req }) => {
-            try {
-                const { status, user } = await getUser(req);
-                if (!status) return { req };
-                return { user, req };
-            } catch (err) {
-                logger.warn(`${req.ip}: ${err.message}, Token: ${req.headers.authorization}`);
-                return { req };
-            }
-        },
-        validationRules: [depthLimit(
+const server = new ApolloServer({
+    resolvers: {
+        ...scalarResolvers,
+        ...mergeResolvers(
+            loadFilesSync(path.join(__dirname, './resolvers/**/*.js'))
+        ),
+    },
+    typeDefs: [
+        print(loadFilesSync(path.join(__dirname, './typedefs/**/*.graphql'))),
+        ...scalarTypeDefs,
+    ],
+    csrfPrevention: true,
+    // TODO: Add Proper Error Handling.
+    cache: 'bounded',
+    context: async ({ req }) => {
+        try {
+            const { status, user } = await getUser(req)
+            if (!status) return { req }
+            return { user, req }
+        } catch (err) {
+            logger.warn(
+                `${req.ip}: ${err.message}, Token: ${req.headers.authorization}`
+            )
+            return { req }
+        }
+    },
+    validationRules: [
+        depthLimit(
             10,
             {}, // ignore no fields
-            depth => { if (depth >= 10) logger.warn(`Depth Limit Exceeded: ${depth}`) }
-        )]
-    })
-    return server
-})();
+            (depth) => {
+                if (depth >= 10) logger.warn(`Depth Limit Exceeded: ${depth}`)
+            }
+        ),
+    ],
+})
+
+server.start().then(()=> {
+    server.applyMiddleware({ app: require("../app") })
+})
 
