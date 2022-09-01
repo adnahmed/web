22423fd06ddd0cc51034114b19b51733d5aca43f
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-09-01 08:58:40 +0500

    Make .env.dev defuault config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 switch (process.env.NODE_ENV.toLowerCase()) {
-    case 'debug':
-        module.exports = require('./config.dev')
-        break
     case 'production':
         module.exports = require('./config.prod')
+        break
+    default:
+    case 'debug':
+        module.exports = require('./config.dev')
         break
 }
