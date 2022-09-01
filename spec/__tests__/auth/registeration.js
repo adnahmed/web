df5487534248876058fc1bf6146f1f669feb1ac9
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-09-01 09:01:04 +0500

    Create setupDB context for tests

--- spec/__tests__/auth/registeration.js
+++ spec/__tests__/auth/registeration.js
@@ -1,7 +1,14 @@
-test ("2 + 2 equals 4", () => {
-    expect(2+2).toBe(4);
-})
+const { setupDB } = require('../../utils/context')
+describe('Registeration Tests', () => {
+    beforeEach(() => {
+        setupDB()
+    })
 
-test("2 + 2 does not equal 3", () => {
-    expect(2+3).not.toBe(3);
-});
+    test('2 + 2 equals 4', () => {
+        expect(1 + 2).toBe(4)
+    })
+
+    test('2 + 2 does not equal 3', () => {
+        expect(2 + 3).not.toBe(3)
+    })
+})
--- spec/utils/context.js
+++ spec/utils/context.js
@@ -2,6 +2,11 @@ const neo4j = require("../../src/db/neo4j");
 const cypher = require("../../src/db/cypher");
 module.exports = {
     setupDB: () => {
-        neo4j.write(cypher("create-or-replace-db"));
+        switch (process.env.NEO4J_ENTERPRISE.toLowerCase()) {
+            case "true":
+                neo4j.write(cypher("create-or-replace-db"));
+            case "false":
+                neo4j.write(cypher("delete-all-nodes-and-relationships"))
+        }
     }
 }
