f0da1c69dd67ed57d4f16f06944e41def8d278bf
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-09-03 23:22:47 +0500

    Use debug config default

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 require('dotenv').config()
-switch (process.env.NODE_ENV.toLowerCase()) {
+switch (process.env.NODE_ENV || 'debug') {
     case 'production':
         module.exports = require('./config.prod')
         break
-    default:
     case 'debug':
         module.exports = require('./config.dev')
         break
