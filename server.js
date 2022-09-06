6de66005344d605c9a979f0f4f0faef0716cdd8b
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-09-06 08:22:41 +0500

    Start Graphql server independently of express

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const terminusOptions = require('./src/terminus');
 const { createTerminus } = require('@godaddy/terminus');
 const http = require('http');
 const api = http.createServer(app).listen(config.port)
+
 api.on('listening', () => {
     console.log(`Express Server is listening on port ${config.port}`);
-    server.start().then(()=> {
-        server.applyMiddleware({ app: app })
-        console.log("GraphQL Server started.")
-    })
 }) 
+
+server.start().then(()=> {
+    server.applyMiddleware({ app: app })
+    console.log("GraphQL Server started.")
+})
+
 createTerminus(api, terminusOptions)
 module.exports = api;
 
