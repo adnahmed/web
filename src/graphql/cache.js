76f8a44fb09d1c15f45f2e179b8eafe5cdd9898d
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-09-11 22:52:27 +0500

    Initiate keyv cache

---
+++ src/graphql/cache.js
@@ -0,0 +1,11 @@
+const KeyvRedis = require('@keyv/redis')
+const Keyv = require('keyv')
+const redisClient = require('./redis')
+
+const keyvRedis = new KeyvRedis(redisClient)
+const cache = new Keyv({ store: keyvRedis })
+cache.on('error', handleRedisConnection)
+function handleRedisConnection(error) {
+    logger.error(error.message)
+}
+module.exports = cache
