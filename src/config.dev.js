d2271a510995091ada34fa1e26bda63a0a094a67
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-09-11 20:04:03 +0500

    Add minio package dependency

--- src/config.dev.js
+++ src/config.dev.js
@@ -1,28 +1,28 @@
 const config = {
-    SECRET: 'mysecret',
-    SALT_ROUNDS: 1,
-    PORT: 4001,
-    NEO4J: {
-        PATH: '.env.dev',
+    secret: 'mysecret',
+    saltRounds: 1,
+    port: 4001,
+    neo4j: {
+        path: '.env.dev',
     },
-    REDIS: {
-        USERNAME: 'default',
-        PASSWORD: '',
+    redis: {
+        username: 'default',
+        password: '',
         REDIS_HOST: 'localhost',
         REDIS_PORT: 7379,
         MAX_CONNECTION_RETRY: 10,
         RECONNECT_TIME: 3000,
     },
-    LIVEKIT: {
-        API_KEY: 'APItrPRpw93WoiW',
-        SECRET_KEY: 'CSNYtif1ll1nvoeeqpV7mcMMKI7fQs34vSJ3BQLhUPJA',
-        HOST: 'http://localhost:8880',
+    livekit: {
+        apiKey: 'APItrPRpw93WoiW',
+        secretKey: 'CSNYtif1ll1nvoeeqpV7mcMMKI7fQs34vSJ3BQLhUPJA',
+        host: 'http://localhost:8880',
     },
 }
 
 module.exports = {
     ...config,
-    GRAPHQL: {
-        ENDPOINT: `http://localhost:${config.PORT}/graphql`,
+    graphql: {
+        endpoint: `http://localhost:${config.port}/graphql`,
     },
 }
